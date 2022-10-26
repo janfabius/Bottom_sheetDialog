ec2d011a61964b03d07f3e55a559b10ee266ed57
janfabius <adanet18@gmail.com>
2022-10-26 17:52:01 +0200

    mod per guiscan

--- app/src/main/java/com/example/bottomsheetlayout/MainActivity.java
+++ app/src/main/java/com/example/bottomsheetlayout/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         button_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDialog_promo();
+                showDialog_promo("2021","2022",10,"394,56 pz\n~ 2 ct","565,78 pz\n~ 5 ct","0.98","0.99");
             }
         });
 
@@ -70,13 +71,41 @@ public class MainActivity extends AppCompatActivity {
         dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 
-    private void showDialog_promo() {
+    private void showDialog_promo(String annoPrecedente, String annoCorrente, int mediaGG, String resMediaGG1, String resMediaGG2, String PrezzoMedio1, String PrezzoMedio2) {
 
         final Dialog dialog = new Dialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.bottomsheetlayout);
 
         ImageView image_close = dialog.findViewById(R.id.sh_close);
+        TextView anno1 =  dialog.findViewById(R.id.textView_anno1);
+        TextView anno2 =  dialog.findViewById(R.id.textView_anno2);
+        TextView media_ris1 =  dialog.findViewById(R.id.textView_risMediaAnno1);
+        TextView media_ris2 =  dialog.findViewById(R.id.textView_risMediaAnno2);
+        TextView mediaGG1 =  dialog.findViewById(R.id.textView_mediaAnno1);
+        TextView mediaGG2 =  dialog.findViewById(R.id.textView_mediaAnno2);
+        TextView prezzoMedio1 =  dialog.findViewById(R.id.textView_prezzoMedioAnno1);
+        TextView prezzoMedio2 =  dialog.findViewById(R.id.textView_prezzoMedioAnno2);
+
+        TextView incremento =  dialog.findViewById(R.id.textView_incremento);
+
+        anno1.setText(annoPrecedente);
+        anno2.setText(annoCorrente);
+        media_ris1.setText(resMediaGG1);
+        media_ris2.setText(resMediaGG2);
+        mediaGG1.setText("Media "+String.valueOf(mediaGG)+" giorni");
+        mediaGG2.setText("Media "+String.valueOf(mediaGG)+" giorni");
+        prezzoMedio1.setText("Prezzo medio: "+PrezzoMedio1);
+        prezzoMedio2.setText("Prezzo medio: "+PrezzoMedio2);
+
+
+        if(false){
+            incremento.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_keyboard_double_arrow_up_24, 0, 0, 0);
+        }else{
+            incremento.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_keyboard_double_arrow_down_24, 0, 0, 0);
+        }
+        incremento.setText("- 15,32 %");
+
 
         image_close.setOnClickListener(new View.OnClickListener() {
             @Override
