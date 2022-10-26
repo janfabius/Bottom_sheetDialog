d3d4a2a101f42d8f04d131987b796448e460117e
janfabius <adanet18@gmail.com>
2022-10-26 16:29:33 +0200

    mod per guiscan

--- app/src/main/java/com/example/bottomsheetlayout/MainActivity.java
+++ app/src/main/java/com/example/bottomsheetlayout/MainActivity.java
@@ -20,47 +20,71 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bottomsheet;
+    Button button_1, button_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomsheet = findViewById(R.id.botttom_sheet);
-        bottomsheet.setOnClickListener(new View.OnClickListener() {
+
+        //button che richiama la dialog
+        button_1 = findViewById(R.id.button1);
+        button_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                showDialog();
-
+                showDialog_promo();
             }
         });
 
+        button_2 = findViewById(R.id.button2);
+        button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog_artLegati();
+            }
+        });
+
+
+
     }
 
-    private void showDialog() {
+    private void showDialog_artLegati() {
 
         final Dialog dialog = new Dialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.bottomsheetlayout);
 
-
         ImageView image_close = dialog.findViewById(R.id.sh_close);
-
 
         image_close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 dialog.dismiss();
-                Toast.makeText(MainActivity.this,"chiudo",Toast.LENGTH_SHORT).show();
-
             }
         });
 
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
 
+    }
 
+    private void showDialog_promo() {
 
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottomsheetlayout);
+
+        ImageView image_close = dialog.findViewById(R.id.sh_close);
+
+        image_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
 
         dialog.show();
         dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
